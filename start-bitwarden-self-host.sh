95e7affcd037be86f6c936eb901a46439cb2df7c
alextorquin <alextorquin@gmail.com>
2020-08-24 22:53:22 +0200

    .

--- start-bitwarden-self-host.sh
+++ start-bitwarden-self-host.sh
@@ -5,5 +5,5 @@
 # Arranque de los contenedores definidos en docker-compose.yml
 sudo docker-compose up -d
 
-# COnfiguración de re-arranque automatico para todos los contenedores que componen Bitwarden-Self-Hosted
+# Configuración de re-arranque automatico para todos los contenedores que componen Bitwarden-Self-Hosted
 docker update --restart always bw_backup bitwarden_self_host_bitwarden_1 bitwarden_self_host_nginx_1
