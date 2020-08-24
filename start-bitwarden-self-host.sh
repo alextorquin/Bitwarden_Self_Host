b955b753e32c74b10eb5c82c30146704c66009d7
alextorquin <alextorquin@gmail.com>
2020-08-24 22:46:58 +0200

    Commit inicial a mi propio repo

---
+++ start-bitwarden-self-host.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Este script debe encontrarse en la ruta base del proyecto
+
+# Arranque de los contenedores definidos en docker-compose.yml
+sudo docker-compose up -d
+
+# COnfiguración de re-arranque automatico para todos los contenedores que componen Bitwarden-Self-Hosted
+docker update --restart always bw_backup bitwarden_self_host_bitwarden_1 bitwarden_self_host_nginx_1
