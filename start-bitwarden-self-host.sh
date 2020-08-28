e58ed9770dcfba87ec7eed1ad962c315c97ac5c7
alextorquin <alextorquin@gmail.com>
2020-08-28 23:23:43 +0200

    Mejoras en el script de arranque

--- start-bitwarden-self-host.sh
+++ start-bitwarden-self-host.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 
-# Este script debe encontrarse en la ruta base del proyecto.
+# 1/ Parada de contenedores que puedan estar en ejecucion
+docker stop bw_backup bitwarden_self_host_bitwarden_1 bitwarden_self_host_nginx_1 bitwarden
 
-# Arranque de los contenedores definidos en docker-compose.yml
-sudo docker-compose up -d
+# 2.a/ Creacion de las imagenes definidad en docker-compose.yml si es necesario. 
+# (Desactivado por defecto. Esta orden es exclusiva con la numero 2.b y para ajecutarla ste script debe encontrarse en la ruta base del proyecto.)
+#sudo docker-compose up -d
 
-# Configuración de re-arranque automatico para todos los contenedores que componen Bitwarden-Self-Hosted.
+# 2.b/ Arranque de los contenedores definidos en docker-compose.yml. (Exclusiva con numero 2.a) 
+docker start bw_backup bitwarden_self_host_bitwarden_1 bitwarden_self_host_nginx_1
+
+# 3/ Configuración de re-arranque automatico para todos los contenedores que componen Bitwarden-Self-Hosted.
 docker update --restart always bw_backup bitwarden_self_host_bitwarden_1 bitwarden_self_host_nginx_1
